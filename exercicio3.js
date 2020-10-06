000615556013b071a73e242ac7ae7e094aa46925
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-10-06 18:52:58 -0300

    Delete exercicio3.js

--- exercicio3.js
+++
@@ -1,8 +0,0 @@
-var skills = ["Javascript", "ReactJS", "React Native"];
-
-//o indexOf retorna -1 se não possuir o elemento, logo != -1 retorna o que possui igual.
-function temHabilidade(skills) {
-  return skills.indexOf("Javascript") != -1;
-}
-
-console.log(temHabilidade(skills));
